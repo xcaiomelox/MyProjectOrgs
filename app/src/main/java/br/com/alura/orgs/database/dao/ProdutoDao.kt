3b77f3a1d4e757fce65d70afe35d6e3fe1c2ad15
Caio <caio.melo51@gmail.com>
2022-12-16 11:50:15 -0300

    ✨ Adicionando Entidades e DAO

---
+++ app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
@@ -0,0 +1,19 @@
+package br.com.alura.orgs.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import br.com.alura.orgs.model.Produto
+
+
+@Dao
+interface ProdutoDao {
+
+    @Query("SELECT * FROM Produto")
+    fun buscaTodos(): List<Produto>
+
+    @Insert
+    fun salva (vararg Produto: Produto)
+
+
+}
